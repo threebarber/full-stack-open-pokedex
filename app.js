afd1a2b8cc7bdbbbd14c6725b717266875df40a0
threebarber <19513830+threebarber@users.noreply.github.com>
2024-02-11 22:13:06 -0500

    e

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ const PORT = process.env.PORT || 5000;
 // Serve static files from the 'dist' directory
 app.use(express.static(staticDir));
 
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(staticDir, "index.html"));
+});
+
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
