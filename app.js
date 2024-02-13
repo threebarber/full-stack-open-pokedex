02cedfee401141ba9132551720dfb60749b9da98
threebarber <19513830+threebarber@users.noreply.github.com>
2024-02-13 16:23:56 -0500

    new

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 });
 
 app.get('/version', (req, res) => {
-  res.send('6') // change this string to ensure a new version deployed
+  res.send('7') // change this string to ensure a new version deployed
 })
 
 app.get('/health', (req, res) => {
